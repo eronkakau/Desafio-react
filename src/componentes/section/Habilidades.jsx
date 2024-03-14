44471ba922d5238b90de6e6270effcfee6624086
eronkakau <eranio51@gmail.com>
2024-03-14 13:33:16 -0400

    aplicando efeito na minha apresentação na function Habilidades

--- src/componentes/section/Habilidades.jsx
+++ src/componentes/section/Habilidades.jsx
@@ -1,13 +1,51 @@
 import ButtonA from '../elements/ButtonA'
 import styles from './Habilidades.module.css'
+import { useEffect, useState } from 'react'
 
 
 function Habilidades(){
 
+    const [text, setText] = useState('');
+    const toRotate = ['Chrislen Eron ','Desenvolvedor front-end', 'Analista de Sistemas'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setInDeleting] = useState(false);
+    const period = 180;
+    const [delta, setDelta] = useState(100)
+    
+
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+
+    },[text])
+     
+    const toType = ()=>{
+    let i = loop % toRotate.length;
+    let fullText = toRotate[i]
+    let updateText = isDeleting ? fullText.substring(0,text.length-1): fullText.substring(0,text.length+1)
+    
+    setText(updateText);
+
+    if(!isDeleting && updateText === fullText){
+      setInDeleting(true);
+      setDelta(period);
+    }else if(isDeleting && updateText === ''){
+
+        setInDeleting(false);
+        setDelta(period);
+        setLoop(loop+1);
+    }
+
+
+    }
+
     return(
         <div className={styles.tec} id='habilidades'>
-        <p>Olá me chamo Chrislen Eron, sou formado <br />
-            em Sistemas de Informação, tenho 33 anos, <br />
+        
+        <p>Olá me chamo {text}, tenho 33 anos, <br />
             ao longo da minha vida sempre procuro <br />
             desafios novos, como por exemplo em me <br />
             tornar programador. Tenho habilidades em <br />
